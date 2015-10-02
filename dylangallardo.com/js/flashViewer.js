f5cd47ccd07a7dc0625fecd2ac96242ca8966820
Dylan Gallardo <gallardodylan42@gmail.com>
2015-10-02 02:07:37 -0400

    Added functionality to flash viewer (.gif swap)
    - The flash content viewer on the character particle system page now
    dynamically swaps the flash content to .gif versions if flash isn't
    detected on the device.
    - minor change to the audio volume for the button click.
    - minor change to the font for the "Dylan Gallardo" text in the
    navigation bar when viewed on mobile.

--- dylangallardo.com/js/flashViewer.js
+++ dylangallardo.com/js/flashViewer.js
@@ -1,24 +1,43 @@
-var characterList = ["flash/DGallardo_RunningGirl_Side.swf",
-	"flash/DGallardo_Character_Zenfella.swf",
-	"flash/DGallardo_Character_Bill.swf",
-	"flash/DGallardo_Character_Fatfella.swf",
-	"flash/DGallardo_Character_TiredGirl.swf",
-	"flash/DGallardo_Character_MagicGuy.swf",
-	"flash/DGallardo_Character_RunningGirl.swf",
-	"flash/DGallardo_Character_RunningGirl_Views.swf",];
-
+var characterList = null;
 var currentContent = 0;
 var parent = $('#flash-content embed').parent();
-// var newElement = "<embed scr='" + characterList[current += 1] + "' align='center' width='100%' height='100%'>";
 var buttonLeft = document.getElementById("button-left");
 var buttonRight = document.getElementById("button-right");
 
-function changeContent() {
-	newElement = "<embed src='" + characterList[currentContent] + "' align='center' width='100%' height='100%'>";
+
+// Check for flash player in this browser.
+// If not available switch to .gif versions of content
+// *swfobject relies on the swfobject.js resource*
+if(swfobject.hasFlashPlayerVersion("1"))
+{
+	var characterList = [
+		"flash/DGallardo_RunningGirl_Side.swf",
+		"flash/DGallardo_Character_Zenfella.swf",
+		"flash/DGallardo_Character_Bill.swf",
+		"flash/DGallardo_Character_Fatfella.swf",
+		"flash/DGallardo_Character_TiredGirl.swf",
+		"flash/DGallardo_Character_MagicGuy.swf",
+		"flash/DGallardo_Character_RunningGirl.swf",
+		"flash/DGallardo_Character_RunningGirl_Views.swf"
+		];
+}
+else
+{
+	var characterList = [
+		"img/Particle_Characters/Character_ZenFella.gif",
+		"img/Particle_Characters/Character_BigFella.gif",
+		"img/Particle_Characters/Character_Bill.gif",
+		"img/Particle_Characters/Character_TiredGirl.gif",
+		"img/Particle_Characters/Character_MagicFella.gif"
+	];
+
+	newElement = "<embed src='" + characterList[0] + "' align='center' width='100%' height='100%'>";
 	$('#flash-content embed').remove();
 	parent.append(newElement);
 }
 
+
+// Add event listeners
 buttonLeft.addEventListener("click", function() {
 	currentContent--;
 	if (currentContent < 0) {
@@ -28,8 +47,7 @@ buttonLeft.addEventListener("click", function() {
 	changeContent();
 });
 
-buttonRight.addEventListener("click", function() { 
-	console.log("clicked");
+buttonRight.addEventListener("click", function() {
 	currentContent++;
 	if (currentContent >= characterList.length) {
 		currentContent = 0;
@@ -37,3 +55,10 @@ buttonRight.addEventListener("click", function() {
 
 	changeContent();
 });
+
+
+function changeContent() {
+	newElement = "<embed src='" + characterList[currentContent] + "' align='center' width='100%' height='100%'>";
+	$('#flash-content embed').remove();
+	parent.append(newElement);
+}
--- dylangallardo.com/js/flipAudio.js
+++ dylangallardo.com/js/flipAudio.js
@@ -1,6 +1,8 @@
 $("#button-left")[0].addEventListener("click", playSound);
 $('#button-right')[0].addEventListener("click", playSound);
 
+document.getElementById('flip-sound').volume = 0.5;
+
 function playSound() {
 	console.log("left clicked");
 	$('#flip-sound')[0].play();
