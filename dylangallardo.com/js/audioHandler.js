7b7077a4be0ba2dc9d218fc8fb28ef1b406677f6
Dylan Gallardo <gallardodylan42@gmail.com>
2015-10-09 21:45:48 -0400

    W3C validation updates + audio update
    - Nearly all of the pages have been renamed and modified so that they
    meet some of the basic standards for W3C validation.
    - audioHandler.js has been updated so that it dynamically creates the
    html audio tags. Currently it just creates all of the audio tags needed
    across all pages (even if that page doesn't use it), but this should be
    changed soon. The original hardcoded html audio tags have also been
    removed of course.

--- dylangallardo.com/js/audioHandler.js
+++ dylangallardo.com/js/audioHandler.js
@@ -3,16 +3,32 @@ var ambientVolume = 0;
 var ambientAudio = null;
 var mouse = {x: null, y: null, oldX: null, oldY: null, movementDist: 0};
 
+// create the audio elements for this page
+createAudioSource('audio/mouseMove.wav', 'audio/mouseMove.mp3',
+  'mouse-move-sound', true, true);
+createAudioSource('audio/hover.wav', 'audio/hover.mp3', 
+  'hover-sound');
+createAudioSource('audio/nav_hover.wav', 'audio/nav_hover.mp3', 
+  'nav-hover-sound');
+createAudioSource('audio/viewFlip.wav', 'audio/viewFlip.mp3',
+  'flip-sound');
+
 if ($("#mouse-move-sound").length > 0) {
   ambientAudio = document.getElementById('mouse-move-sound');
   ambientAudio.volume = ambientVolume;
   ambientAudio.play();
 
   // HTML5 audio loop wasn't working so did it manually
-  ambientAudio.addEventListener('ended', function() {
-      this.currentTime = 0;
-      this.play();
-  }, false);
+  if (typeof document.getElementById('mouse-move-sound').loop == 'boolean')
+  {
+    document.getElementById('mouse-move-sound').loop = true;
+  }
+  else {
+    ambientAudio.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+  }
 
   // Events and listeners
   $(window).mousemove(function(e){
@@ -31,7 +47,7 @@ if ($("#mouse-move-sound").length > 0) {
     mouse.oldY = mouse.y;
 
     // change volume
-    ambientVolume += volumeModifier; //0.04 is a good speed
+    ambientVolume += 0.04; //0.04 is a good speed
     if (ambientVolume > 0.5) {
       ambientVolume = 0.5;
     }
@@ -49,8 +65,8 @@ addSoundToAll('.navbar-nav li a', '#nav-hover-sound', 0.8);
 addSoundToAll('.img-contact-icon', '#hover-sound', 1);
 
 
-// Adds a sound that will trigger on hover to all
-// elements with this id/class
+//Adds a sound that will trigger on hover to all
+//elements with this id/class
 function addSoundToAll(element, sound, startVolume) {
   i=0; //reset counter
   var soundPrefixRemoved = sound.substr(1);
@@ -74,6 +90,36 @@ function addSoundToAll(element, sound, startVolume) {
         $(sound + $(this).data("count"))[0].play();
       });
   }
+}
+
+function createAudioSource(pathWav, pathMp3, audioId, loop, autoplay) {
+  // dynamic audio init
+  var audio = document.createElement('audio');
+  if (loop) {
+    audio.loop = true;
+  }
+  if (autoplay) {
+    audio.autoplay = true;
+  }
+  audio.id = audioId;
+  var source = document.createElement('source');
+  
+  if (audio.canPlayType('audio/wav;')) {
+      source.type = 'audio/wav';
+      source.src = pathWav;
+  }
+  else {
+      source.type = 'audio/mpeg';
+      source.src = pathMp3;
+  }
+
+  audio.appendChild(source);
+  
+  // you should insert this html later
+  // Your browser does not support the <code>audio</code> element.
+  document.body.appendChild(audio);
+
+  return audio;
 }
 
 update();
